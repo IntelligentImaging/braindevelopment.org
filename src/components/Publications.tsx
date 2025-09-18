3ff9c9bdebc23aa87fb7c19a4e778fc9cffdb3cc
Rylee Kubo <kubor@hs.uci.edu>
2025-09-18 08:59:41 -0700

    Update Publications.tsx

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -205,7 +205,7 @@ const Publications = () => {
           </h2>
         </div>
 
-        <div className="grid gap-6 max-w-4xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-6xl mx-auto">
           {publications.map((paper, index) => (
             <Card
               key={index}
